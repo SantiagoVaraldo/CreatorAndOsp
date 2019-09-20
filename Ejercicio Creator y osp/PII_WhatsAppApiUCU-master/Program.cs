28375140cd45e6e2efa6a6b1ee7c5154bbcbf0b9
Santiago Varaldo <santivaraldo02@gmail.com>
2019-09-20 18:33:38 -0300

    commit initial

---
+++ Ejercicio Creator y osp/PII_WhatsAppApiUCU-master/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace WhatsAppApiUCU
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        { 
+            const string accountSid = "ACa8395e7223e7719988c09db8dfb68f0f";
+            const string authToken = "fdcaac12dc7e5b031908429ee402e7a4";
+
+            var whatsApp = new WhatsAppApi(accountSid, authToken);
+            string sid = whatsApp.Send("+598<Poné tu teléfono acá>", "Hey there! I'm using WhatsAppUCU");
+            Console.WriteLine(sid);
+        }
+    }
+}
